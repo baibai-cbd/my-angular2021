ba2258ecc547308d53153eb427d8d6f0f11cc67c
Yuhao Dong <ydong119@gmail.com>
2021-10-16 09:17:58 +0800

    prep for course project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
