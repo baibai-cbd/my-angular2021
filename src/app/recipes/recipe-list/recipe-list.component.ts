daaaec8b9f6f222c207ccf8d2f4b2f11fab3ab64
Yuhao Dong <ydong119@gmail.com>
2021-10-19 16:19:10 +0800

    worked on recipe-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('recipe1', 'shrimp taco', 'https://live.staticflickr.com/6155/6268151954_be4b584bbe_b.jpg'),
+    new Recipe('a', 'b', 'c')
+  ];
 
   constructor() { }
 
